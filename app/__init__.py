ee07128826ef496cb8934306d67ca9a81d225d09
1-Megan-1 <moconnor3@ithaca.edu>
2023-11-09 11:49:35 -0500

    Bootstrap implemented along with error pages

--- app/__init__.py
+++ app/__init__.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
+from flask_bootstrap import Bootstrap
 from config import Config
 
 app = Flask(__name__)
@@ -10,4 +11,6 @@ db = SQLAlchemy(app)
 migrate = Migrate(app, db, render_as_batch=True)
 login = LoginManager(app)
 login.login_view = 'login'
-from app import routes, models 
+bootstrap = Bootstrap(app)
+
+from app import routes, models, errors
